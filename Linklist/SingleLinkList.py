11c40a394e3b1b9bf50a58df4ff0c4a966c84278
smzizi021 <smzizi021@student.wethinkcode.co.za>
2021-12-11 08:45:56 +0200

    Single Link list

--- Linklist/SingleLinkList.py
+++ Linklist/SingleLinkList.py
@@ -19,18 +19,19 @@ class single_link_list():
             node = node.ref
 
 
-    def insert(self,value):
-        location = 0
+    def insert(self,value, location = 0):
+       
         new_node = Node(value)
-        if  self.head is None:
+        if  self.head is None and self.tail is None:
             self.head = new_node
             self.tail = new_node
-            print(self.tail.value)
         else:
             if location == 0:
                 new_node.ref = self.head
                 self.head = new_node
-                print(self.tail)
+            elif location == -1:
+                self.tail.ref = new_node
+                self.tail = new_node
             
             
     def get_all(self):
@@ -40,9 +41,11 @@ class single_link_list():
            node = self.head
            count = 0
            while node is not None:
-                print(f"{count} <> node.value")
+                print(f"{count} <> {node.value}")
                 node = node.ref
+                count += 1
    
+
     def search(self,value):
         if self.head is None:
             return "Link List is Empty"
@@ -54,14 +57,32 @@ class single_link_list():
                 node = node.ref
             return f"{value} Not Found"
 
-single_link_list = single_link_list()
 
-node = Node(1)
-single_link_list.head = node
+    def delete_all(self):
+        self.head = None
+        self.tail = None
+
+
+    def reverse(self):
+        if self.head is None:
+            print("Link List is Empty")
+        else:
+            prev = None
+            current = self.head
+            next_ = None
+            while current is not None:
+                next_ = current.ref
+                current.ref = prev
+                prev = current
+                current = next_
+            self.head = prev
+        
+        
+single_link_list = single_link_list()
 
 single_link_list.insert(12)
 single_link_list.insert(134)
-single_link_list.insert(1000)
+single_link_list.insert(1000,-1)
 single_link_list.insert(9999)
 single_link_list.insert(1234)
 
@@ -69,6 +90,11 @@ single_link_list.insert(1234)
 h = [node.value for node in single_link_list]
 print(h)
 print(single_link_list.search(9999))
+single_link_list.get_all()
+print(">>>>>>> REVERSED <<<<<<<<<<")
+single_link_list.reverse()
+single_link_list.get_all()
+
 
 
 
